a22b9cf2b61a6a6dfbb3399407ce64dadcb982fe
ilya53nov <ilya53nov@yandex.ru>
2024-07-30 00:01:47 +0300

    Создание страницы для участка упаковки

--- production-planning-system/src/pages/packaging-page/packaging-page.tsx
+++ production-planning-system/src/pages/packaging-page/packaging-page.tsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
+import PackagingCurrentOrdersComponent from "../../components/packaging-components/current-orders-component/current-orders-component";
+import PackagingCompletedOrdersComponent from "../../components/packaging-components/completed-orders-component/completed-orders-component";
+
 const PackagingPage: React.FC = () => {
+
+  const [currentTab, setCurrentTab] = useState('current-orders');
+
   return (
-    <div>PackagingPage</div>
+    <div>
+      PackagingPage
+      <ul>
+        <li>
+          <button onClick={() =>setCurrentTab('current-orders')}>current-orders</button>
+        </li>
+        <li>
+        <button onClick={() =>setCurrentTab('completed-orders')}>completed-orders</button>
+        </li>
+      </ul>
+
+      {currentTab === 'current-orders' ? <PackagingCurrentOrdersComponent /> : ''}
+      {currentTab === 'completed-orders' ? <PackagingCompletedOrdersComponent /> : ''}
+    </div>
+
   )
 }
 
